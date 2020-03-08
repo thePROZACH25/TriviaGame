49b1bae689cdf6a20ae36adddf02a41fa86a984e
Zach S.L <zach.s.ledford@gmail.com>
2020-03-08 09:04:03 -0400

    added questions append

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,29 @@
-$("#start-button").on("click", function() {
-  $("#start-button").remove();
-  for (var i = 0; i < questions.length; i++) {
-    $(".quest-table").append(`<h3> ${questions[i].question} </h3>`);
-  }
-});
-
+// ========================================
+// Var
 var questions = [
   {
     question:
       "What geothermal Icelandic site has the same name as a 1980 movie?",
-    answers: ["Xanadu", "The Shining", "Heaven’s Gate", "The Blue Lagoon"],
+    answers: ["Xanadu", "The Shining", "Heavens Gate", "The Blue Lagoon"],
     currectAnswers: "The Blue Lagoon"
   }
 ];
+
+// ========================================
+// Start game function
+$("#start-button").on("click", function() {
+  $("#start-button").remove();
+  for (var i = 0; i < questions.length; i++) {
+    $(".quest-table").append(`<h3> ${questions[i].question} </h3>`);
+    for (var j = 0; j < questions[i].answers.length; j++) {
+      $(".quest-table").append(
+        "<input type='radio' name='question-" +
+          i +
+          "' value='" +
+          questions[i].answers[j] +
+          "'>" +
+          questions[i].answers[j]
+      );
+    }
+  }
+});
