104f183671fa7230e97b2e822d955a3f4fefcb75
Zach S.L <zach.s.ledford@gmail.com>
2020-03-08 08:45:31 -0400

    added questions append

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
-$("#start-button").on("click",function(){
-    $(".quest-table").remove();
-})
+$("#start-button").on("click", function() {
+  $("#start-button").remove();
+  for (var i = 0; i < questions.length; i++) {
+    $(".quest-table").append(`<h3> ${questions[i].question} </h3>`);
+  }
+});
+
+var questions = [
+  {
+    question:
+      "What geothermal Icelandic site has the same name as a 1980 movie?",
+    answers: ["Xanadu", "The Shining", "Heaven’s Gate", "The Blue Lagoon"],
+    currectAnswers: "The Blue Lagoon"
+  }
+];
